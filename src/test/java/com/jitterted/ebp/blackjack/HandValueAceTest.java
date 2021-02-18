42f414e6b53e63eb1bd356ef0a8d795d01290d8d
Ted M. Young <tedyoung@gmail.com>
2021-02-18 11:05:18 -0800

    Lab #5: Extracted Hand to solve the Primitive Obsession code smell.

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -1,7 +1,5 @@
 package com.jitterted.ebp.blackjack;
 
-import com.jitterted.ebp.blackjack.Card;
-import com.jitterted.ebp.blackjack.Game;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -14,22 +12,22 @@ public class HandValueAceTest {
 
   @Test
   public void handWithOneAceTwoCardsIsValuedAt11() throws Exception {
-    Game game = new Game();
-    var hand = List.of(new Card(DUMMY_SUIT, "A"),
+    List<Card> cards = List.of(new Card(DUMMY_SUIT, "A"),
                        new Card(DUMMY_SUIT, "5"));
+    Hand hand = new Hand(cards);
 
-    assertThat(game.handValueOf(hand))
+    assertThat(hand.value())
         .isEqualTo(11 + 5);
   }
 
   @Test
   public void handWithOneAceAndOtherCardsEqualTo11IsValuedAt1() throws Exception {
-    Game game = new Game();
-    var hand = List.of(new Card(DUMMY_SUIT, "A"),
+    List<Card> cards = List.of(new Card(DUMMY_SUIT, "A"),
                        new Card(DUMMY_SUIT, "8"),
                        new Card(DUMMY_SUIT, "3"));
+    Hand hand = new Hand(cards);
 
-    assertThat(game.handValueOf(hand))
+    assertThat(hand.value())
         .isEqualTo(1 + 8 + 3);
   }
 
